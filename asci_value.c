ebaf36f8c07d5502e41230e8e9b2f8913598f58f
vaibhavbuccha <jainvaibhav415@gmail.com>
2018-08-10 18:13:21 +0530

    Create asci_value.c

---
+++ asci_value.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+
+//program to print asci value of a character.
+
+{
+	int c;
+	printf("enter the character\n");
+	scanf("%c",&c);
+	printf("the asci value of %c is:%d",c,c);
+	return 0;
+}
