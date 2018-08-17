fa8c308701ef0e6b40faaaa75b13adba4eea4b8d
vaibhavbuccha <jainvaibhav415@gmail.com>
2018-08-17 23:31:07 +0530

    Create reverse_number.c

---
+++ reverse_number.c
@@ -0,0 +1,27 @@
+/*_______________________________________________________|
+|program to print reverse number.                        |
+|        number = 1234                                   |
+|         while (number!=0){                             |
+|        number % 10 = 4=reminder                        |
+|         reverse(0) = reverse*10+reminder               |
+|          number = number/10}                           |
+|________________________________________________________|*/
+#include<stdio.h>
+int main()
+{
+	 int n, reversedNumber = 0, remainder;
+
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+    while(n != 0)
+    {
+        remainder = n%10;
+        reversedNumber = reversedNumber*10 + remainder;
+        n /= 10;
+    }
+
+    printf("Reversed Number = %d", reversedNumber);
+
+    return 0;
+}
