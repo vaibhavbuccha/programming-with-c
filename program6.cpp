2bb6c64b8657a017c19a3d14ce90af9ce6c10656
vaibhav buccha <jainvaibhav415@gmail.com>
2019-11-20 04:30:36 +0530

    Create program6.cpp

---
+++ program6.cpp
@@ -0,0 +1,44 @@
+/*
+	6. Code for displaying nodes of linked list
+*/
+#include <stdio.h> 
+#include <stdlib.h> 
+  
+struct Node { 
+    int data; 
+    struct Node* next; 
+}; 
+  
+
+void printList(struct Node* n) 
+{ 
+    while (n != NULL) { 
+        printf(" %d ", n->data); 
+        n = n->next; 
+    } 
+} 
+  
+int main() 
+{ 
+    struct Node* head = NULL; 
+    struct Node* second = NULL; 
+    struct Node* third = NULL; 
+  
+    
+    head = (struct Node*)malloc(sizeof(struct Node)); 
+    second = (struct Node*)malloc(sizeof(struct Node)); 
+    third = (struct Node*)malloc(sizeof(struct Node)); 
+  
+   	head->data = 6; 
+    head->next = second; 
+  
+    second->data = 45; 
+    second->next = third; 
+  
+    third->data = 78; 
+    third->next = NULL; 
+  
+    printList(head); 
+  
+    return 0; 
+}
